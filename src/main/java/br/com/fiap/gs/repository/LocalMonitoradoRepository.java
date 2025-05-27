973a18339d207aab5497b37ee6b4290510caded7
guiakiraa <guilhermeakira0810@gmail.com>
2025-05-27 12:11:14 -0300

    feat: LocalMonitoradoRepository feito

---
+++ src/main/java/br/com/fiap/gs/repository/LocalMonitoradoRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.gs.repository;
+
+import br.com.fiap.gs.model.LocalMonitorado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocalMonitoradoRepository extends JpaRepository<LocalMonitorado, Long> {
+}
