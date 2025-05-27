e4e83a731d57345e4b5001a4bb39642be2f81582
guiakiraa <guilhermeakira0810@gmail.com>
2025-05-27 10:25:31 -0300

    feat: configurações das dependenciais e application properties

--- src/main/java/br/com/fiap/gs/GsApplication.java
+++ src/main/java/br/com/fiap/gs/GsApplication.java
@@ -2,7 +2,13 @@ package br.com.fiap.gs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableCaching
+@EnableJpaRepositories
+@EntityScan
 @SpringBootApplication
 public class GsApplication {
 
