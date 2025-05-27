64d5ad0227baab2743a02d7e137a85dcf594c62d
guiakiraa <guilhermeakira0810@gmail.com>
2025-05-27 12:11:48 -0300

    feat: SensorLixoRepository feito

---
+++ src/main/java/br/com/fiap/gs/repository/SensorLixoRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.gs.repository;
+
+import br.com.fiap.gs.model.SensorLixo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SensorLixoRepository extends JpaRepository<SensorLixo, Long> {
+}
